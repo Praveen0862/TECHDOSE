4b72f8d76633157b291cc7792428724bcfde070f
Praveen0862 <98651362+Praveen0862@users.noreply.github.com>
2022-09-08 09:03:55 +0530

    Create SetIthBit.java

---
+++ BitManipulation/HomeWork/SetIthBit.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class SetIthBit
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int pos = sc.nextInt();
+		System.out.println(a|(1 << (pos - 1)));
+	}
+}
