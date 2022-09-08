6c6cc87489db124fc451ff652f37316690ac4223
Praveen0862 <98651362+Praveen0862@users.noreply.github.com>
2022-09-08 08:14:41 +0530

    Create countSetBit.java

---
+++ BitManipulation/HomeWork/countSetBit.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class countSetBit 
+{
+    public static void main (String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int count = 0;
+        while (a > 0)
+        {
+            if ((a&1) == 1)
+            {
+                count++;
+            }
+            a = a >> 1;
+        }
+        System.out.println(count);
+    }
+}
