a43842ddfd0a596cb77f7543d2a135b3e523b253
Praveen0862 <98651362+Praveen0862@users.noreply.github.com>
2022-09-08 08:22:43 +0530

    Update flipIthBit.java

--- BitManipulation/HomeWork/flipIthBit.java
+++ BitManipulation/HomeWork/flipIthBit.java
@@ -1,16 +1,12 @@
 import java.util.*;
 
-public class swap
+public class flipIthBit
 {
     public static void main (String [] args)
     {
         Scanner sc = new Scanner (System.in);
         int x = sc.nextInt();
-        int y = sc.nextInt();
-        System.out.println("Before Swapping : " + x + " " + y);
-        x = x^y;
-        y = y^x; // y^(x^y)
-        x = x^y; // (x^y)^y
-        System.out.println("After Swapping : " + x + " " + y);
+        int pos = sc.nextInt();
+        System.out.println(x^(1 << (pos-1)));
     }
 }
