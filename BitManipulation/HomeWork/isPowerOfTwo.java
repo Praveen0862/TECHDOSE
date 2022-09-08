6ad33539d48c78bc4ba7bb1e2e5bb90273eec692
Praveen0862 <98651362+Praveen0862@users.noreply.github.com>
2022-09-08 08:15:30 +0530

    Create isPowerOfTwo.java

---
+++ BitManipulation/HomeWork/isPowerOfTwo.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class isPowerOfTwo 
+{
+    public static void main (String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a == 0)
+        {
+            System.out.println("NO");
+        }
+        else
+        {
+            System.out.println(((a&(a-1)) == 0) ? "YES" : "NO");
+        }
+    }
+}
