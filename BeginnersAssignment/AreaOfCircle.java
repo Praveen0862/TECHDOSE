7ad389c42b2e31b1a019af6bbae064c503bfb8c4
Praveen0862 <98651362+Praveen0862@users.noreply.github.com>
2022-09-09 14:23:21 +0530

    Create AreaOfCircle.java

---
+++ BeginnersAssignment/AreaOfCircle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class AreaOfCircle
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int r = sc.nextInt();
+		System.out.println(22*r*r/7);
+	}
+
+}
