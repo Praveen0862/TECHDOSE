1640e4f231421e871a93bb0c2fdd3e5f0e63adeb
Praveen0862 <98651362+Praveen0862@users.noreply.github.com>
2022-09-09 14:34:57 +0530

    Update SumOfFirstNNaturalNumbers.java

--- BeginnersAssignment/SumOfFirstNNaturalNumbers.java
+++ BeginnersAssignment/SumOfFirstNNaturalNumbers.java
@@ -3,7 +3,7 @@ public class SumOfFirstNNaturalNumbers
 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n10 = sc.nextInt();
+		int n = sc.nextInt();
 		System.out.println((n*(n+1))/ 2);
 	}
 
