07b60017ab1cc4d4a4e73780ea509f3d8e1fb579
Praveen0862 <98651362+Praveen0862@users.noreply.github.com>
2022-09-09 15:43:40 +0530

    Create PatternPrintingSecond.java

---
+++ BeginnersAssignment/PatternPrintingSecond.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class PatternPrintingSecond
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = N; i > 0; i--)
+		{
+		    for (int j = i; j > 0; j--)
+		    {
+		        System.out.print(j);
+		    }
+		    System.out.println();
+		}
+	}
+}
