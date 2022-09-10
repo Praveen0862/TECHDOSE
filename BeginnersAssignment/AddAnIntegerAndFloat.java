ea0d3fc80da161058c3c2626c7829c1caf4fa2ef
Praveen0862 <98651362+Praveen0862@users.noreply.github.com>
2022-09-10 20:03:08 +0530

    Create AddAnIntegerAndFloat.java

---
+++ BeginnersAssignment/AddAnIntegerAndFloat.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class AddAnIntegerAndFloat
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		float b = sc.nextFloat();
+		System.out.println(a+b);
+	}
+}
