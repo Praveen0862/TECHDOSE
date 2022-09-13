fe4897d70706242ef1dbc773e2194d29c6f55be0
Praveen0862 <98651362+Praveen0862@users.noreply.github.com>
2022-09-13 09:47:56 +0530

    Create SelectionSort.java

---
+++ SortingAlgorithms/SelectionSort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class SelectionSort
+{
+    public static void main (String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int []arr = new int[N];
+        for (int i = 0;i < N; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < N; i++)
+        {
+            int min = arr[i],minInd = i;
+            for (int j = i+1; j < N; j++)
+            {
+                if (arr[j] < min)
+                {
+                    min = arr[j];
+                    minInd = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minInd];
+            arr[minInd] = temp;
+        }
+        for (int j = 0; j < N; j++)
+        {
+            System.out.print(arr[j] + " ");
+        }
+    }
+}
