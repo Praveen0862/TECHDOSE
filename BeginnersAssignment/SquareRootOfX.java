1d4ffe63d56a23c5cd71d157b587a7b64380af5a
Praveen0862 <98651362+Praveen0862@users.noreply.github.com>
2022-09-16 12:14:38 +0530

    Update SquareRootOfX.java

--- BeginnersAssignment/SquareRootOfX.java
+++ BeginnersAssignment/SquareRootOfX.java
@@ -1,10 +1,22 @@
 class Solution {
     public int mySqrt(int x) {
-        int i = 1;
-        while (i*i <= x)
+        int start = 1,end = x;
+        if (x == 0)
         {
-            i++;
+            return 0;
         }
-        return i-1;
+        while (true)
+        {
+            int mid = start + (end-start)/2;
+            if (mid > x/mid) 
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                if ((mid+1) > x/(mid+1)) return mid;
+                else start = mid+1;
+            }
+        }
     }
 }
