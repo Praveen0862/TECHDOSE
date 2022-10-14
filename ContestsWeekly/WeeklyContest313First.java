4a74e9fd3db6404b73bfaf465ea7d97dec54497f
Praveen0862 <98651362+Praveen0862@users.noreply.github.com>
2022-10-14 15:23:26 +0530

    Create WeeklyContest313First.java

---
+++ ContestsWeekly/WeeklyContest313First.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int min = (a < b) ? a : b;
+        int count = 0,i;
+        for (i = 1; i <= min; i++)
+        {
+            if (a%i == 0 && b%i == 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
