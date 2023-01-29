cfcd8a989c2b17530ddcfc1b585d4a506277fd41
Praveen0862 <98651362+Praveen0862@users.noreply.github.com>
2023-01-29 20:01:18 +0530

    Update SortColors.java

--- Array/Assignments/SortColors.java
+++ Array/Assignments/SortColors.java
@@ -1,16 +1,29 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int length = nums.length;
-        for (int i = 0; i < length; i++)
+        int low = 0,high = nums.length-1,mid = 0,num;
+        while (mid <= high)
         {
-            for (int j = i+1; j < length; j++)
+            if (nums[mid] == 2)
             {
-                if (nums[i] > nums[j])
-                {
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
+                num = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = num;
+                high--;
+            }
+            else if (nums[mid] == 1)
+            {
+                num = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = num;
+                mid++;
+            }
+            else
+            {
+                num = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = num;
+                low++;
+                mid++;
             }
         }
     }
